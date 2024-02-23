2c537bfb094959a8dbf80233d3037e8d366c95ba
Aguilar.304743 <Aguilar.304743@LAB-A-PC34>
2024-02-23 10:00:16 +0800

    Add project files.

---
+++ StudentGradeApplication/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace StudentGradeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.English = new System.Windows.Forms.Label();
+            this.Math = new System.Windows.Forms.Label();
+            this.Science = new System.Windows.Forms.Label();
+            this.Filipino = new System.Windows.Forms.Label();
+            this.History = new System.Windows.Forms.Label();
+            this.StudentName = new System.Windows.Forms.TextBox();
+            this.EnglishG = new System.Windows.Forms.TextBox();
+            this.MathG = new System.Windows.Forms.TextBox();
+            this.ScienceG = new System.Windows.Forms.TextBox();
+            this.FilipinoG = new System.Windows.Forms.TextBox();
+            this.HistoryG = new System.Windows.Forms.TextBox();
+            this.GenerateBut = new System.Windows.Forms.Button();
+            this.View = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(70, 49);
+            this.Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(52, 16);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grades";
+            // 
+            // English
+            // 
+            this.English.AutoSize = true;
+            this.English.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.English.Location = new System.Drawing.Point(70, 127);
+            this.English.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.English.Name = "English";
+            this.English.Size = new System.Drawing.Size(62, 16);
+            this.English.TabIndex = 2;
+            this.English.Text = "English:";
+            // 
+            // Math
+            // 
+            this.Math.AutoSize = true;
+            this.Math.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Math.Location = new System.Drawing.Point(70, 169);
+            this.Math.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Math.Name = "Math";
+            this.Math.Size = new System.Drawing.Size(44, 16);
+            this.Math.TabIndex = 3;
+            this.Math.Text = "Math:";
+            // 
+            // Science
+            // 
+            this.Science.AutoSize = true;
+            this.Science.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Science.Location = new System.Drawing.Point(69, 209);
+            this.Science.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Science.Name = "Science";
+            this.Science.Size = new System.Drawing.Size(67, 16);
+            this.Science.TabIndex = 4;
+            this.Science.Text = "Science:";
+            // 
+            // Filipino
+            // 
+            this.Filipino.AutoSize = true;
+            this.Filipino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Filipino.Location = new System.Drawing.Point(70, 251);
+            this.Filipino.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Filipino.Name = "Filipino";
+            this.Filipino.Size = new System.Drawing.Size(62, 16);
+            this.Filipino.TabIndex = 5;
+            this.Filipino.Text = "Filipino:";
+            // 
+            // History
+            // 
+            this.History.AutoSize = true;
+            this.History.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.History.Location = new System.Drawing.Point(70, 293);
+            this.History.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(60, 16);
+            this.History.TabIndex = 6;
+            this.History.Text = "History:";
+            // 
+            // StudentName
+            // 
+            this.StudentName.Location = new System.Drawing.Point(182, 44);
+            this.StudentName.Margin = new System.Windows.Forms.Padding(4);
+            this.StudentName.Name = "StudentName";
+            this.StudentName.Size = new System.Drawing.Size(451, 22);
+            this.StudentName.TabIndex = 7;
+            // 
+            // EnglishG
+            // 
+            this.EnglishG.Location = new System.Drawing.Point(182, 127);
+            this.EnglishG.Margin = new System.Windows.Forms.Padding(4);
+            this.EnglishG.Name = "EnglishG";
+            this.EnglishG.Size = new System.Drawing.Size(80, 22);
+            this.EnglishG.TabIndex = 8;
+            // 
+            // MathG
+            // 
+            this.MathG.Location = new System.Drawing.Point(182, 169);
+            this.MathG.Margin = new System.Windows.Forms.Padding(4);
+            this.MathG.Name = "MathG";
+            this.MathG.Size = new System.Drawing.Size(80, 22);
+            this.MathG.TabIndex = 9;
+            // 
+            // ScienceG
+            // 
+            this.ScienceG.Location = new System.Drawing.Point(182, 209);
+            this.ScienceG.Margin = new System.Windows.Forms.Padding(4);
+            this.ScienceG.Name = "ScienceG";
+            this.ScienceG.Size = new System.Drawing.Size(80, 22);
+            this.ScienceG.TabIndex = 10;
+            // 
+            // FilipinoG
+            // 
+            this.FilipinoG.Location = new System.Drawing.Point(182, 251);
+            this.FilipinoG.Margin = new System.Windows.Forms.Padding(4);
+            this.FilipinoG.Name = "FilipinoG";
+            this.FilipinoG.Size = new System.Drawing.Size(80, 22);
+            this.FilipinoG.TabIndex = 11;
+            // 
+            // HistoryG
+            // 
+            this.HistoryG.Location = new System.Drawing.Point(182, 293);
+            this.HistoryG.Margin = new System.Windows.Forms.Padding(4);
+            this.HistoryG.Name = "HistoryG";
+            this.HistoryG.Size = new System.Drawing.Size(80, 22);
+            this.HistoryG.TabIndex = 12;
+            // 
+            // GenerateBut
+            // 
+            this.GenerateBut.Location = new System.Drawing.Point(75, 341);
+            this.GenerateBut.Margin = new System.Windows.Forms.Padding(4);
+            this.GenerateBut.Name = "GenerateBut";
+            this.GenerateBut.Size = new System.Drawing.Size(189, 62);
+            this.GenerateBut.TabIndex = 13;
+            this.GenerateBut.Text = "Generate Average";
+            this.GenerateBut.UseVisualStyleBackColor = true;
+            this.GenerateBut.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // View
+            // 
+            this.View.AutoSize = true;
+            this.View.BackColor = System.Drawing.SystemColors.Control;
+            this.View.Location = new System.Drawing.Point(423, 209);
+            this.View.Name = "View";
+            this.View.Size = new System.Drawing.Size(0, 16);
+            this.View.TabIndex = 14;
+            this.View.Visible = false;
+            this.View.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 569);
+            this.Controls.Add(this.View);
+            this.Controls.Add(this.GenerateBut);
+            this.Controls.Add(this.HistoryG);
+            this.Controls.Add(this.FilipinoG);
+            this.Controls.Add(this.ScienceG);
+            this.Controls.Add(this.MathG);
+            this.Controls.Add(this.EnglishG);
+            this.Controls.Add(this.StudentName);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.Filipino);
+            this.Controls.Add(this.Science);
+            this.Controls.Add(this.Math);
+            this.Controls.Add(this.English);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Name);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label English;
+        private System.Windows.Forms.Label Math;
+        private System.Windows.Forms.Label Science;
+        private System.Windows.Forms.Label Filipino;
+        private System.Windows.Forms.Label History;
+        private System.Windows.Forms.TextBox StudentName;
+        private System.Windows.Forms.TextBox EnglishG;
+        private System.Windows.Forms.TextBox MathG;
+        private System.Windows.Forms.TextBox ScienceG;
+        private System.Windows.Forms.TextBox FilipinoG;
+        private System.Windows.Forms.TextBox HistoryG;
+        private System.Windows.Forms.Button GenerateBut;
+        private System.Windows.Forms.Label View;
+    }
+}
+
---
+++ StudentGradeApplication/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Student_name = Convert.ToString(Name.Text);
+            double EnglishGrade = Convert.ToDouble(EnglishG.Text);
+            double MathGrade = Convert.ToDouble(MathG.Text);
+            double ScienceGrade = Convert.ToDouble(ScienceG.Text);
+            double FilipinoGrade = Convert.ToDouble(FilipinoG.Text);
+            double HistoryGrade = Convert.ToDouble(HistoryG.Text);
+
+            double AverageGrade = ( EnglishGrade + MathGrade + ScienceGrade + FilipinoGrade + HistoryGrade) / 5;
+            View.Visible = true;
+            View.Text = AverageGrade >= 75.00 ? "The Student Passed The General Average " + Student_name + " Is " + AverageGrade : "The Student failed " + AverageGrade;
+            
+            
+            
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
